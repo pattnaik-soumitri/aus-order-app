d1472b44ea1444aea45ec4c06fd9adf612334f44
pattnaik-soumitri <soumitri.pattnaik@gmail.com>
2023-06-20 17:35:08 +0530

    - added page header in order entry page

--- src/views/OrderEntry.vue
+++ src/views/OrderEntry.vue
@@ -45,6 +45,7 @@ const submit = async () => {
     <section>
         <div class="grid">
             <div class="order-form">
+                <h6>Order Entry</h6>
                 <form @submit.prevent="submit">
                     <label for="customer_name">
                         Customer Name
