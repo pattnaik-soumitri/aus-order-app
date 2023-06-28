5c11d97c0246024a0d68f51fe901f6104912cd20
pattnaik_soumitri <soumitri.2.pattnaik@shoppertrak.com>
2023-06-28 16:18:36 +0530

    - decreased login header font size

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -26,7 +26,7 @@ const handleLogin = async () => {
         <form class="login-form" @submit.prevent="handleLogin">
             <article>
                 <header>
-                    <h3>Login</h3>
+                    <h5>Login</h5>
                 </header>
 
                 <label for="email">Email</label>
